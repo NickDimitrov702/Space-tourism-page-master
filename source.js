3aa2a84218425c7dbeab378764e37d7a4b86281e
Nickolay Dimitrov <dimitrov.nikolai9224@gmail.com>
2022-01-31 22:39:15 +0200

    Page on reload to stay with the background for the relevant page.

--- source.js
+++ source.js
@@ -1,10 +1,14 @@
-import {html, render} from './node_modules/lit-html/lit-html.js'
+// Check why it shows invalid URL some times on e.target.href - it may be the event since it is covering 2 nested elements.
+// Make router for Planets navigation, it will be some kind of a nested router.
+// Seperate the destinations components like, so you can render them with the destination router.
+// Most probably you will need a layout like the initial one that is made for the main navigation. 
+import { html, render } from './node_modules/lit-html/lit-html.js'
 
 
 
 let h1 = document.getElementsByTagName('h1')[0]
 let planetsList = document.querySelectorAll('#planets-list')
-// let [moon, mars, eurpa, titan] = planetsList[0].children
+
 
 import home from './pages/home.js'
 import layout from './pages/layout.js'
@@ -26,58 +30,68 @@ const routes = [
 // Simple router having array with routes that includes objects with rout path and the path's template to be rendered
 // router functions as a manager of routs, we add a navigationHandler functions so onClick we could navigate to the links withing the NavBar
 const router = (path) => {
-    
+    // Last time I forgot to compare x.path with path, instead I was comparing path === path :(
+    let url = new URL('http://localhost:3000'+`${path}`);
+    // history.pushState(null, null, url)
+    // Checking isnide the router pathname if it is the same as the page we are on so we can keep the background 
+    // for the relevant page the same on refresh.
+
+    if(url.pathname === '/destionationMars'){
+        let background = document.getElementById('root')
+        // setTimeout(background.style.backgroundImage = 'url(./assets/destination/background-destination-desktop.jpg)', 2000)
+        background.style.backgroundImage = 'url(./assets/destination/background-destination-desktop.jpg)'
+
+    } if(url.pathname === '/') {
+        let background = document.getElementById('root')
+        background.style.backgroundImage = 'url(./assets/home/background-home-desktop.jpg)'
+    }
+    // You can delete below console.log
+    console.log('http://localhost:3000'+`${path}`)
     let rout = routes.find(x => x.path == path) || routes.find(x => x.path == '/not-found')
-    // let context = rout.context;
-    render(layout(rout.tempalte, { navigationHandler }),document.getElementById('app'))
-    
+    render(layout(rout.tempalte, { navigationHandler }), document.getElementById('app'))
+
 }
 
-
 router(location.pathname)
-console.log(location.pathname)
+// console.log(location.pathname)
 
 function navigationHandler(e) {
     // e.preventDefault()
-    if(e.target.tagName == 'A') {
+    if (e.target.tagName == 'A') {
         e.preventDefault()
         console.log(e.target.href)
 
-        // if(e.target.href === 'destionationMars') {
-           
-        // }
-        
     }
 
 
 
     let url = new URL(e.target.href);
-    history.pushState(null,null, url)
-    console.log(location + `${url.pathname}`)
+    // let background = document.getElementById('root')
+    // background.style.backgroundImage = 'url(./assets/home/background-home-desktop.jpg)'
+    // history.pushState(null,null, url)
+    // console.log(location.pathname)
 
-    if(url.pathname === '/destionationMars'){
-
-        // let destinationBackground = './assets/destination/background-destination-desktop.jpg';
+    if (url.pathname === '/destionationMars') {
+        let url = new URL(e.target.href);
+        console.log(url)
+        history.pushState(null, null, url)
         let background = document.getElementById('root')
         background.style.backgroundImage = 'url(./assets/destination/background-destination-desktop.jpg)'
+        router(url.pathname)
+
     }
 
-    if(url.pathname === '/') {
+    if (url.pathname === '/') {
+        let url = new URL(e.target.href);
+        history.pushState(null, null, url)
         let background = document.getElementById('root')
         background.style.backgroundImage = 'url(./assets/home/background-home-desktop.jpg)'
+        router(url.pathname)
     }
 
 
-   router(url.pathname)
-    
+    // router(url.pathname)
+
 }
 
 
-
-// const navigatTo = url => {
-//     url = location.pathname
-//     window.history.pushState(null, null, url)
-//     router()
-// }
-
-// navigatTo()
